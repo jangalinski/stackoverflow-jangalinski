fff5483bfa333e5a5b2924369df7d707717c5b37
jangalinski <jan.galinski@holisticon.de>
2013-07-28 21:57:55 +0200

    generate enum

--- src/test/java/de/github/jangalinski/codemodel/GenerateEnumTest.java
+++ src/test/java/de/github/jangalinski/codemodel/GenerateEnumTest.java
@@ -1,36 +1,72 @@
 package de.github.jangalinski.codemodel;
 
+import static com.sun.codemodel.JExpr._this;
+import static com.sun.codemodel.JExpr.lit;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Test;
 
-import com.google.common.collect.FluentIterable;
 import com.google.common.io.Files;
+import com.sun.codemodel.ClassType;
+import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JEnumConstant;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
 
 public class GenerateEnumTest {
 
+    private static final int PRIVATE_FINAL = JMod.PRIVATE + JMod.FINAL;
     private final JCodeModel codeModel = new JCodeModel();
     private final File tempDir = Files.createTempDir();
 
     @Test
     public void shouldGenerateEnumWithConstructor() throws Exception {
-        JDefinedClass definedClass = codeModel._class("ReportColumns");
+        JDefinedClass definedClass = codeModel.rootPackage()._class(JMod.PUBLIC, "ReportColumns", ClassType.ENUM);
+        
+        JFieldVar field1 = definedClass.field(PRIVATE_FINAL, String.class, "column");
+        JFieldVar field2 = definedClass.field(PRIVATE_FINAL, Boolean.class, "filterable");
+        JFieldVar field3 = definedClass.field(PRIVATE_FINAL, Boolean.class, "includeInHavingClause");
+        
+        JMethod constructor = definedClass.constructor(JMod.PRIVATE);
+        JVar param1 = constructor.param(String.class, "column");
+        JVar param2 = constructor.param(Boolean.class, "filterable");
+        JVar param3 = constructor.param(Boolean.class, "includeInHavingClause");
+
+        JBlock body = constructor.body();
+        body.assign(_this().ref(field1), param1);
+        body.assign(_this().ref(field2), param2);
+        body.assign(_this().ref(field3), param3);
+        
+        JEnumConstant enumMonth = definedClass.enumConstant("MONTH");
+        enumMonth.arg(lit("month"));
+        enumMonth.arg(lit(true));
+        enumMonth.arg(lit(false));
+
+        JEnumConstant enumDay = definedClass.enumConstant("DAY");
+        enumDay.arg(lit("day"));
+        enumDay.arg(lit(false));
+        enumDay.arg(lit(true));
+
         codeModel.build(tempDir);
         printGeneratedClass();
     }
 
     private void printGeneratedClass() throws IOException {
-        System.out.println(Files.readLines(tempDir.listFiles()[0], Charset.defaultCharset()));
+        System.out.println(tempDir.getAbsolutePath());
+        System.out.println(Files.toString(tempDir.listFiles()[0], Charset.defaultCharset()));
     }
 
     @After
     public void cleanUp() {
+        tempDir.listFiles()[0].delete();
         tempDir.delete();
     }
 
